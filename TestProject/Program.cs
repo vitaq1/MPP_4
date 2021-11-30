8e73ebcef191c33b894c8305989114056c1cd4a2
lancy <vshust505@gmail.com>
2021-11-30 23:39:19 +0300

    Добавьте файлы проекта.

---
+++ TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            await new Pipeline().Generate(2,
+                @"C:\Users\User\Desktop\СПП4\GeneratedTests",
+                @"C:\Users\User\Desktop\СПП4\TestProject\TestClass.cs",
+                @"C:\Users\User\Desktop\СПП4\TestProject\AnotherTestClass.cs"); 
+        }
+    }
+}
